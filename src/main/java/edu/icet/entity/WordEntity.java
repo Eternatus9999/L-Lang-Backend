7ea853336e09e104193e2380af874f247e178f9b
Eternatus9999 <eternatus9999@gmail.com>
2024-10-25 02:05:09 +0530

    add Word class and WordEntity class to the project with attributes

---
+++ src/main/java/edu/icet/entity/WordEntity.java
@@ -0,0 +1,20 @@
+package edu.icet.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity(name = "word")
+public class WordEntity {
+    @Id
+    private long id;
+    private String name;
+    private String pronunciation;
+}
